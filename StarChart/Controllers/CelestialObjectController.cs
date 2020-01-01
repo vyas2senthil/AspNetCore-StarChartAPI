f54dffc615ec4eca64659c518d7577029f13a489
vyas senthil <vyas2senthil@gmail.com>
2020-01-01 13:17:16 +1000

    made small correction to fix controller.

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -7,15 +7,15 @@ using StarChart.Data;
 
 namespace StarChart.Controllers
 {
-    [ApiController]
     [Route("")]
-    public class CelestialObjectController : Controller
+    [ApiController]
+    public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
-        public CelestialObjectController(ApplicationDbContext dbContext)
+        public CelestialObjectController(ApplicationDbContext context)
         {
-            _context = dbContext;
+            _context = context;
         }
     }
 }
